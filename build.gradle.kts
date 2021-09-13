32443d6a65381282de1d2a037d523be691831429
StageGuard <1355416608@qq.com>
2021-09-13 19:46:28 +0800

    0.4.6

--- build.gradle.kts
+++ build.gradle.kts
@@ -6,7 +6,7 @@ plugins {
 }
 
 group = "KonnyakuCamp"
-version = "0.4.4"
+version = "0.4.6"
 
 repositories {
     mavenLocal()
--- src/main/kotlin/me/stageguard/sctimetable/PluginMain.kt
+++ src/main/kotlin/me/stageguard/sctimetable/PluginMain.kt
@@ -24,7 +24,7 @@ import me.stageguard.sctimetable.service.*
 object PluginMain : KotlinPlugin(
     JvmPluginDescription(
         id = "me.stageguard.sctimetable",
-        version = "0.4.4",
+        version = "0.4.6",
         name = "SuperCourseTimetable"
     )
 ) {
